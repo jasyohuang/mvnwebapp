9b3f895876977552d4b4ea96349c974f0f79aba4
Jason Yonathan Huang <jasonyonathan@live.com>
2020-07-17 15:19:29 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -16,16 +16,28 @@ os.system('powershell -Command "(gc var.txt) -replace \'PATH\', \'%CD%\' | Out-F
 ini=ffile.find('NODE1PATH')+(len('NODE1PATH')+1)
 rest=ffile[ini:]
 search_enter=rest.find('\n')
-node1 = rest[:search_enter]
+node1 = rest[:search_enter] 
 
 ini=ffile.find('NODE2PATH')+(len('NODE2PATH')+1)
 rest=ffile[ini:]
 search_enter=rest.find('\n')
 node2 = rest[:search_enter]
 
+ini=ffile.find('IPSERVICE1')+(len('IPSERVICE1')+1)
+rest=ffile[ini:]
+search_enter=rest.find('\n')
+ip1 = rest[:search_enter]
+
+ini=ffile.find('IPSERVICE2')+(len('IPSERVICE2')+1)
+rest=ffile[ini:]
+search_enter=rest.find('\n')
+ip2 = rest[:search_enter]
+
 path=os.getcwd()
 
 if(path==node1):
 	os.system('powershell -Command "(gc var.txt) -replace \'CLUSTER_NODE\', \'NODE 1\' | Out-File -encoding ASCII var.txt"')
+	os.system('powershell -Command "(gc var.txt) -replace \'IPSERVICE\', \''+ip1+'\' | Out-File -encoding ASCII var.txt"')
 else:
 	os.system('powershell -Command "(gc var.txt) -replace \'CLUSTER_NODE\', \'NODE 2\' | Out-File -encoding ASCII var.txt"')
+	os.system('powershell -Command "(gc var.txt) -replace \'IPSERVICE\', \''+ip2+'\' | Out-File -encoding ASCII var.txt"')
